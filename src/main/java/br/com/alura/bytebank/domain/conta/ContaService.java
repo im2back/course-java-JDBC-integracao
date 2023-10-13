91e881d0389eefb58fd557ffd7878fd74001f19e
Jefferson Richards <jefferson.richards@outlook.com>
2023-10-13 16:07:27 -0300

    PreparedStatment

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -1,14 +1,26 @@
 package br.com.alura.bytebank.domain.conta;
 
+import br.com.alura.bytebank.ConnectionFactory;
 import br.com.alura.bytebank.domain.RegraDeNegocioException;
 import br.com.alura.bytebank.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ContaService {
+import com.mysql.cj.xdevapi.PreparableStatement;
 
+
+public class ContaService {
+	private ConnectionFactory connection;
+	
+	ContaService(){
+		this.connection = new ConnectionFactory();
+	}
+	
     private Set<Conta> contas = new HashSet<>();
 
     public Set<Conta> listarContasAbertas() {
@@ -26,8 +38,30 @@ public class ContaService {
         if (contas.contains(conta)) {
             throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
         }
-
-        contas.add(conta);
+        
+        //comando sql 
+        String sql = "INSERT INTO conta (numero, saldo, cliente_nome, cliente_cpf_ cliente_email)" + "VALUES (?, ?, ?, ?, ?)";
+        
+       //Abrindo conexão com a database 			
+        Connection conn = connection.recuperarConexao();
+        
+        
+        try {
+            //metodo para setar os valores pendentes ????
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            
+            preparedStatement.setInt(1, conta.getNumero());
+            preparedStatement.setBigDecimal(2, BigDecimal.ZERO);
+            preparedStatement.setString(3,dadosDaConta.dadosCliente().nome());
+            preparedStatement.setString(4,dadosDaConta.dadosCliente().cpf());
+            preparedStatement.setString(5,dadosDaConta.dadosCliente().email());
+			
+            preparedStatement.execute();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+   
+        
     }
 
     public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
